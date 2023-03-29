3fab5ed16e7b7e7307bdc70634b95fd06d9a057b
Luobo <1933180432@qq.com>
2023-03-29 16:54:57 +0800

    frist commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <svg class="icon" aria-hidden="true">
+    <use xlink:href="#icon-hanbaobao"></use>
+  </svg>
+</template>
+
+<style scoped>
+.icon {
+  width: .5rem;
+  height: .5rem;
+}
+</style>
